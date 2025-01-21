7ad23af08d78ab0234dfc2abbb0abbf5c85da912
Ezira Tigab <ezratgab@gmail.com>
2025-01-21 19:21:45 +0300

    feat: add ennums

---
+++ src/enums/roomTypeEnum.ts
@@ -0,0 +1,14 @@
+export enum RoomType {
+  SINGLE = "single",
+  DOUBLE = "double",
+  TRIPLE = "triple",
+  QUAD = "quad",
+  QUEEN = "queen",
+  KING = "king",
+  TWIN = "twin",
+  STUDIO = "studio",
+  SUITE = "suite",
+  DUPLEX = "duplex",
+  PENTHOUSE = "penthouse",
+  VILLA = "villa",
+}
