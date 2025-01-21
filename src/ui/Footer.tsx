7594baf1832dc055096438de492fdd86df76cd13
Ezira Tigab <ezratgab@gmail.com>
2025-01-21 19:20:51 +0300

    add: add shad cn components

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -1,8 +1,9 @@
 function Footer() {
   return (
-    <footer className="mt-10 flex flex-col items-center justify-start border-t-2 border-slate-300 bg-slate-200 px-10 py-6 capitalize tracking-tighter">
-      <p>&copy; 2024 Copyright: BDHotels.com</p>
-      <span className="mt-2">all rights are reserved</span>
+    <footer className="mt-10 flex  justify-center text-slate-800 items-center flex-row  gap-10 border-t-2 border-slate-300 bg-slate-200 px-10 py-6  tracking-tighter">
+      <p>&copy; 2024 Copyright: hotelify.com</p>
+      <span className="">All Rights are Reserved</span>
+      <span className="text-accent-500">By: NextGenCoders</span>
     </footer>
   );
 }
