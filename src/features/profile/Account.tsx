a2966d86964ed19942421d1c8e743ad1e74ecc8c
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-09 11:07:51 +0300

    feat: update style on account and refactor the account sidebar

--- src/features/profile/Account.tsx
+++ src/features/profile/Account.tsx
@@ -1,46 +1,48 @@
-import { Link, Outlet, useLocation } from "react-router-dom";
-import { VscAccount } from "react-icons/vsc";
-import { FiSettings } from "react-icons/fi";
-import { MdOutlineBookmarkAdded } from "react-icons/md";
+import { Outlet } from "react-router-dom";
+import { useAuthContext } from "../../context/AuthContext";
+import { AccountSidebar } from "./components/AccountSidebar";
+
+export interface IDataAccountSidebar {
+  to: string;
+  pathname: string;
+  text: string;
+}
+
+const dataAccountSidebar: IDataAccountSidebar[] = [
+  {
+    to: "profile",
+    pathname: "/account/profile",
+    text: "My Profile",
+  },
+  {
+    to: "bookings",
+    pathname: "/account/bookings",
+    text: "My Bookings",
+  },
+  {
+    to: "settings",
+    pathname: "/account/settings",
+    text: "Settings",
+  },
+  {
+    to: "identity-verification",
+    pathname: "/account/identity-verification",
+    text: "Identity Verification",
+  },
+];
 
 const Account = () => {
-  const { pathname } = useLocation();
+  // get user from the context
+  const { user } = useAuthContext();
+
+  console.log(user);
 
   return (
-    <div className="m-4 mx-auto flex max-h-[90rem] min-h-screen w-[90vw] max-w-[90rem] gap-2 rounded-xl border-2 shadow-lg">
+    <div className="mx-auto flex min-h-screen w-[90vw] gap-2 rounded-xl bg-light-100">
       {/* SIDE BAR */}
-      <ul className="w-[20%] space-y-2 flex flex-col gap-0 rounded-2xl border-r-2 p-4 text-gray-700 shadow-xl">
-        <li >
-          <Link
-            to="profile"
-            className={`flex w-full items-center gap-4 rounded-md px-4 py-2 text-lg shadow-lg transition-all duration-200 ${pathname === "/account/profile" ? "translate-x-2 font-bold text-accent-500" : "hover:translate-x-2 hover:text-accent-500/90"}`}
-          >
-            <VscAccount size={"24px"} />
-            My Profile
-          </Link>
-        </li>
-        <li >
-          <Link
-            to="settings"
-            className={`flex w-full items-center gap-4 rounded-md px-4 py-2 text-lg shadow-lg transition-all duration-200 ${pathname === "/account/settings" ? "translate-x-2 font-bold text-accent-500" : "hover:translate-x-2 hover:text-accent-500/90"}`}
-          >
-            <FiSettings size="24px" />
-            Settings
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="bookings"
-            className={`mb-5 flex w-full items-center gap-4 rounded-md px-4 py-2 text-lg shadow-lg transition-all duration-200 ${pathname === "/account/bookings" ? "translate-x-2 font-bold text-accent-500" : "hover:translate-x-2 hover:text-accent-500/90"}`}
-          >
-            <MdOutlineBookmarkAdded size="24px" />
-            My Bookings
-          </Link>
-        </li>
-      </ul>
-
+      <AccountSidebar data={dataAccountSidebar} />
       {/* BODY | OUTLET */}
-      <div className="flex-1 rounded-lg p-2 shadow-xl">
+      <div className="flex-1 p-2">
         <Outlet />
       </div>
     </div>
---
+++ src/features/profile/components/AccountSidebar.tsx
@@ -0,0 +1,21 @@
+import { Link, useLocation } from "react-router-dom";
+import { IDataAccountSidebar } from "../Account";
+
+export const AccountSidebar = ({ data }: { data: IDataAccountSidebar[] }) => {
+  const { pathname } = useLocation();
+
+  return (
+    <ul className="flex w-[250px] min-w-[250px] flex-col gap-0 space-y-2 border-r-2 border-light-400 text-gray-700">
+      {data.map((item, i) => (
+        <li key={i}>
+          <Link
+            to={item.to}
+            className={`flex w-full items-center rounded-md px-4 py-2 transition-all duration-200 hover:bg-light-200 ${pathname === item.pathname ? "bg-light-300" : ""}`}
+          >
+            {item.text}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
