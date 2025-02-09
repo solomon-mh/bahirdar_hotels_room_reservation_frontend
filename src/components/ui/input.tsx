cb4df1bf62b0e6e8828c4fecc4a67921c5feee16
Edmealem kassahun <edmealemkassahun@gmail.com>
2025-02-09 11:49:19 +0300

    feat: add Input component and enhance CompleteOnboarding layout

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+import { cn } from "@components/lib/utils";
+
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "bg-transparent file:bg-transparent file:text-slate-950 focus-visible:ring-slate-950 dark:file:text-slate-50 flex h-9 w-full rounded-md border border-slate-200 px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300 md:text-sm",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
--- src/features/profile/CompleteOnboarding.tsx
+++ src/features/profile/CompleteOnboarding.tsx
@@ -1,5 +1,17 @@
 function CompleteOnboarding() {
-  return <div>CompleteOnboarding</div>;
+  return (
+    <section>
+      <div>
+        <h1>Complete Onboarding</h1>
+
+        <ul className="flex flex-col gap-2">
+          <li>
+            <div></div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default CompleteOnboarding;
