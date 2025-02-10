c14ea59290e9fdeb0c6b59d405730519038e87b2
Ezira Tigab <ezratgab@gmail.com>
2025-02-10 08:51:03 +0300

    feat: updating booking status

--- src/features/bookings/BookingDetail.tsx
+++ src/features/bookings/BookingDetail.tsx
@@ -1,28 +1,117 @@
 import { Card, CardContent } from "../../components/ui/card";
 import { Avatar } from "@radix-ui/react-avatar";
-import { useGetBookingByIdQuery } from "../../redux/api/bookingApi";
+import { useDeleteBookingMutation, useGetBookingByIdQuery, useUpdateBookingStatusMutation } from "../../redux/api/bookingApi";
 import { useParams } from "react-router-dom";
 import LoadingPage from "../../pages/utils/LoadingPage";
 import NotFoundPage from "../../pages/utils/NotFoundPage";
 import { Badge } from "../../components/ui/badge";
-import { Button } from "../../components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import ImageSlider from "../../components/Slider";
+import DeleteFeature, { FeatureDeleteActionType } from "../../components/DeleteDialog";
+import ConfirmAction from "../../components/ConfirmDialog";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { BookingStatus } from "../../enums/bookingStatusEnum";
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "../../components/ui/select";
 
 export default function BookingDetails() {
+    const bookingStatuses = Object.values(BookingStatus);
     const { hotelId } = useParams<{ hotelId: string }>();
     const { bookingId } = useParams<{ bookingId: string }>();
     const { data: { data: booking } = {}, isLoading, error } = useGetBookingByIdQuery(bookingId as string);
+    const [confirimAction, { isLoading: updating }] = useUpdateBookingStatusMutation();
+    const [isOpen, setIsOpen] = useState(false);
+    const [status, setStatus] = useState<BookingStatus | null>(booking?.status || null);
+    const onConfirm = () => {
+        try
+        {
+
+            if (!status) return;
+            confirimAction({
+                id: bookingId as string,
+                status: status
+            }).then(() => {
+                toast.success(`Book status from ${booking?.status} to ${status} successfully`);
+                setIsOpen(false);
+            }).catch((err) => {
+                if ('data' in err)
+                {
+                    toast.error(err.data.message);
+                }
+                else
+                {
+                    toast.error(JSON.stringify(err, null, 2));
+                }
+            })
+        } catch (error)
+        {
+            toast.error(JSON.stringify(error, null, 2));
+
+        }
+    };
+
+    useEffect(() => {
+        setStatus(booking?.status || null);
+    }, [booking?.status]);
 
     const isHotelBookingDetail = !!hotelId
     return (
         <div className="flex flex-col gap-4 p-4 ">
-            {!isHotelBookingDetail && <div className="flex  w-full shadow-lg p-2 items-center gap-4">
-                <button onClick={() => window.history.back()} className="p-2 flex items-center gap-2 rounded-lg bg-gray-100">
-                    <ArrowLeft /> back
-                </button>
-                <h1 className="text-2xl font-semibold">Booking Details</h1>
-            </div>}
+            {
+                <div className="flex w-full shadow-lg p-2 items-center justify-between gap-4">
+                    {!isHotelBookingDetail && <div className="flex items-center gap-4">
+                        <button onClick={() => window.history.back()} className="p-2 flex items-center gap-2 rounded-lg bg-gray-100">
+                            <ArrowLeft /> back
+                        </button>
+                        <h1 className="text-2xl font-semibold">Booking Details</h1>
+                    </div>}
+                    <div className="flex flex-1 justify-self-end justify-end  items-center gap-2 px-2">
+                        <div className="flex w-[180px]">
+                            <Select
+                                value={status as string || ""}
+                                onValueChange={(value) => setStatus(value as BookingStatus)}
+
+                            >
+                                <SelectTrigger >
+                                    <SelectValue placeholder="Change Status" className="p-2 rounded-lg text-slate-800 bg-gray-100" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        {/* <SelectLabel>Change Status</SelectLabel> */}
+
+                                        {
+                                            bookingStatuses.map((status, index) => (
+                                                <SelectItem key={index} value={status}>{status}</SelectItem>
+                                            ))
+                                        }
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                        <ConfirmAction
+                            feature="Booking"
+                            featureId={bookingId || ""}
+                            onConfirm={onConfirm}
+                            confirming={updating}
+                            isOpen={isOpen}
+                            setIsOpen={setIsOpen}
+                        />
+                        <DeleteFeature
+                            feature="Booking"
+                            featureId={bookingId || ""}
+                            useDelete={useDeleteBookingMutation as FeatureDeleteActionType}
+                            redirectUrl={`/dashboard${hotelId ? "/hotels/" + hotelId : ""}/bookings`}
+                        />
+                    </div>
+                </div>
+            }
 
             {
                 isLoading && <LoadingPage />
@@ -72,14 +161,14 @@ export default function BookingDetails() {
                             <Card className="p-4">
                                 <CardContent>
                                     <h2 className="text-lg font-semibold">Room Details</h2>
-                                    <p>Room Number: <span className="font-medium">{booking.room.roomNumber}</span></p>
-                                    <p>Type: <span className="font-medium">{booking.room.roomType}</span></p>
-                                    <p>Capacity: <span className="font-medium">{booking.room.capacity} people</span></p>
-                                    <p className="text-gray-500">{booking.room.description}</p>
+                                    <p>Room Number: <span className="font-medium">{booking.room?.roomNumber}</span></p>
+                                    <p>Type: <span className="font-medium">{booking.room?.roomType}</span></p>
+                                    <p>Capacity: <span className="font-medium">{booking.room?.capacity} people</span></p>
+                                    <p className="text-gray-500">{booking.room?.description}</p>
                                     <div className="mt-2">
                                         <h3 className="font-semibold">Facilities:</h3>
                                         <div className="flex flex-wrap gap-2 mt-1">
-                                            {booking.room.roomFacilities.map((facility, index) => (
+                                            {booking.room?.roomFacilities.map((facility, index) => (
                                                 <Badge key={index} className="bg-slate-100 text-accent-500 cursor-pointer hover:bg-slate-200">{facility}</Badge>
                                             ))}
                                         </div>
@@ -91,7 +180,7 @@ export default function BookingDetails() {
                         {/* Room Images */}
                         <div className="mt-6">
                             <h2 className="text-lg font-semibold mb-2">Room Images</h2>
-                            <ImageSlider slidesToShow={2} images={booking.room.images} />
+                            <ImageSlider slidesToShow={2} images={booking.room?.images} />
                         </div>
 
                         {/* Booking Details */}
@@ -115,9 +204,6 @@ export default function BookingDetails() {
                             </Card>
                         </div>
 
-                        <div className="mt-6 flex justify-end">
-                            <Button className="bg-green-500 hover:bg-green-600 text-white px-6 py-2 rounded-lg">Confirm Booking</Button>
-                        </div>
                     </Card>
                 </div>
             }
