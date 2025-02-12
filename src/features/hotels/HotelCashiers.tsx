52a0ef12c4ecbf4e107a57da1b3cb57c04d39edc
Ezira Tigab <ezratgab@gmail.com>
2025-02-12 18:45:30 +0300

    feat: add cashiers

---
+++ src/features/hotels/HotelCashiers.tsx
@@ -0,0 +1,132 @@
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import Search from "../../ui/Search";
+import { useGetAllUsersQuery } from "../../redux/api/userApi";
+import LoadingPage from "../../pages/utils/LoadingPage";
+import NotFoundPage from "../../pages/utils/NotFoundPage";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../../components/ui/table";
+import { CustomPagination } from "@/components/Pagination";
+import { useCreateCashierMutation } from "@/redux/api/hotelApi";
+import { useState } from "react";
+import { IUser } from "@/types/userTypes";
+import { useAuthContext } from "@/context/AuthContext";
+import { AddCashier } from "./components/AddCashier";
+
+function HotelCashiers() {
+    const navigate = useNavigate()
+    const { user } = useAuthContext()
+    const [selectedCashier, setSelectedCashier] = useState<IUser | null>(null)
+    const [searchParams] = useSearchParams();
+    const { data: { data: cashiers, pagination } = {}, isLoading } = useGetAllUsersQuery(searchParams.toString(), { refetchOnMountOrArgChange: true });
+    const [addCashier, { isLoading: adding }] = useCreateCashierMutation();
+
+    const handleAddCashier = () => {
+        addCashier({
+            hotelId: user?.hotel?._id as string,
+            userId: selectedCashier?._id as string
+        })
+    }
+    return (
+        <div className="w-full bg-white  text-gray-600 shadow-md">
+            <div className="flex items-center justify-between p-6">
+                <h1 className="p-4 uppercase">
+                    <Link to="/dashboard/cashiers">All Cashiers</Link>
+                </h1>
+
+                {/* SEARCH  */}
+                <Search />
+
+                <div className="flex items-center justify-between gap-2">
+                    <AddCashier
+                        selectedCashier={selectedCashier}
+                        setSelectedCashier={setSelectedCashier}
+                    />
+                    {selectedCashier && <button
+                        disabled={!selectedCashier || adding}
+                        className="px-4 py-1 border bg-[#34343400] border-accent-500 text-accent-500 hover:bg-accent-500 rounded-md hover:text-slate-100"
+                        onClick={handleAddCashier}
+                    >
+                        Save
+                    </button>}
+                </div>
+            </div>
+
+
+            {isLoading
+                ?
+                (
+                    <LoadingPage />
+                )
+                :
+                !cashiers?.length ?
+                    (
+                        <NotFoundPage >
+                            <p>Cashiers not found</p>
+                        </NotFoundPage>
+                    )
+                    :
+                    (
+                        <div className="flex p-4 flex-col gap-2">
+
+                            <Table className="shadow-md shadow-red-300">
+                                <TableHeader className="bg-slate-200">
+                                    <TableHead>Profile</TableHead>
+                                    <TableHead>First Name</TableHead>
+                                    <TableHead>Last Name</TableHead>
+                                    <TableHead>Email</TableHead>
+                                    <TableHead>Phone number</TableHead>
+                                    <TableHead>Role</TableHead>
+                                    <TableHead>Actions</TableHead>
+                                </TableHeader>
+                                <TableBody className="divide-y divide-gray-200">
+                                    {
+                                        cashiers?.map((user) => (
+                                            <TableRow>
+                                                <TableCell>
+                                                    {
+                                                        user?.profilePicture
+                                                            ? <img
+                                                                src={user?.profilePicture}
+                                                                alt="profile"
+                                                                className="w-7 h-7 rounded-full"
+                                                            />
+                                                            : <div className="w-7 h-7 grid place-items-center" >
+                                                                No profile picture
+                                                            </div>
+                                                    }
+                                                </TableCell>
+                                                <TableCell>{user?.firstName}</TableCell>
+                                                <TableCell>{user?.lastName}</TableCell>
+                                                <TableCell>{user?.email}</TableCell>
+                                                <TableCell>{user?.phoneNumber}</TableCell>
+                                                <TableCell>{user?.role}</TableCell>
+                                                <TableCell>
+                                                    <button
+                                                        className="text-accent-500 hover:underline"
+                                                        onClick={() => {
+                                                            navigate(`/dashboard/cashiers/${user._id}`)
+                                                        }}
+                                                    >
+                                                        View
+                                                    </button>
+                                                </TableCell>
+                                            </TableRow>
+                                        ))
+                                    }
+                                </TableBody>
+                            </Table>
+                            {
+                                pagination && (
+                                    <CustomPagination
+                                        page={pagination?.page}
+                                        totalPages={pagination?.totalPages}
+                                    />
+                                )
+                            }
+                        </div>
+                    )
+            }
+        </div>
+    );
+}
+
+export default HotelCashiers;
---
+++ src/features/hotels/components/AddCashier.tsx
@@ -0,0 +1,104 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
+import { useGetAllUsersQuery } from "@/redux/api/userApi";
+import { IUser } from "@/types/userTypes";
+import Search from "@/ui/Search";
+import Spinner from "@/ui/Spinner";
+import { useState } from "react";
+
+interface Props {
+    selectedCashier: IUser | null;
+    setSelectedCashier: (manager: IUser | null) => void;
+}
+
+export function AddCashier({ selectedCashier, setSelectedCashier }: Props) {
+    const [open, setOpen] = useState(false);
+
+    const { data, isLoading, error } = useGetAllUsersQuery("");
+
+    return (
+        <Drawer open={open} onOpenChange={setOpen}>
+            <DrawerTrigger asChild>
+                <button className="px-4 py-1 border bg-[#34343400] border-accent-500 text-accent-500 hover:bg-accent-500 rounded-md hover:text-slate-100">
+                    {selectedCashier ? "Change Cashier" : "Select manager"}
+                </button>
+            </DrawerTrigger>
+            <DrawerContent className="bg-slate-200 h-[60vh] pb-6 text-slate-900 dark:bg-slate-950 dark:text-slate-50">
+                <DrawerHeader>
+                    <DrawerTitle>List of hotel managers</DrawerTitle>
+                    <DrawerDescription>Select the hotel manager</DrawerDescription>
+                    <Search />
+                </DrawerHeader>
+
+                {isLoading ? (
+                    <div className="p-4 text-center">
+                        <Spinner />
+                    </div>
+                ) : error ? (
+                    <div className="p-4 text-center">
+                        <p className="text-red-500">Failed to load users</p>
+                    </div>
+                ) : !data?.data?.length ? (
+                    <div className="p-4 text-center">
+                        <p className="text-red-500">No users found</p>
+                    </div>
+                ) : (
+                    <div className="p-4 h-[50vh] overflow-y-scroll">
+                        {data.data?.map((user) => (
+                            <div
+                                onClick={() => {
+                                    if (selectedCashier?._id === user._id)
+                                        setSelectedCashier(null);
+                                    else setSelectedCashier(user);
+                                }}
+                                key={user._id}
+                                className={
+                                    "flex items-center p-3 transition-all duration-300 hover:cursor-pointer hover:bg-slate-300" +
+                                    (selectedCashier?._id === user._id ? " border-b-2 border-b-accent-500" : "")
+                                }
+                            >
+                                <div>
+                                    <img
+                                        src={user.profilePicture || "/user1.jpeg"}
+                                        alt=""
+                                        className="mr-2 h-12 w-12 rounded-full object-cover"
+                                    />
+                                </div>
+                                <div className="flex flex-col items-start">
+                                    <h2 className="">
+                                        {user.firstName} {user.lastName}{" "}
+                                        <span className="text-xs text-slate-500">({user.email})</span>
+                                    </h2>
+                                    <p className="text-slate-500">
+                                        {user.phoneNumber}{" "}
+                                        <span className="text-xs text-slate-500">({user.role})</span>
+                                    </p>
+                                </div>
+                                <div className="flex items-center px-10 self-end justify-end flex-1">
+                                    <button className="py-2 bg-accent-500 text-slate-200 px-6 rounded-md hover:text-slate-50">
+                                        Select{selectedCashier?._id === user._id ? "ed" : ""}
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+
+                <DrawerFooter className="flex flex-row justify-end gap-4">
+                    <DrawerClose className="flex" asChild>
+                        <Button
+                            onClick={() => {
+                                setOpen(false);
+                            }}
+                            className="w-full py-2 hover:bg-slate-100 bg-slate-100/90"
+                            variant="outline"
+                        >
+                            Cancel
+                        </Button>
+                    </DrawerClose>
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
+    );
+}
