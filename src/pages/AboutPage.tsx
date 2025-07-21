dcfd079a605700f7863fd0bb463c9a9e71e0493f
solomon-mh <solomon.muhye.wd@gmail.com>
2025-07-21 04:00:50 +0300

    fix: improve layout and styling consistency in AboutPage component

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -2,12 +2,12 @@ function About() {
   return (
     <section className="relative px-5 sm:px-10">
       {/* First Section */}
-      <div className="flex min-h-[80vh] flex-col-reverse items-center gap-6 bg-slate-100 shadow-sm md:flex-row md:p-6">
+      <div className="bg-slate-50 flex min-h-[80vh] flex-col-reverse items-center gap-6 shadow-sm md:flex-row md:p-6">
         {/* Text Content */}
-        <p className="flex flex-1 flex-col gap-6 text-lg sm:text-xl leading-7 tracking-wide text-center md:text-left">
+        <p className="flex flex-1 flex-col gap-6 text-center text-lg leading-7 tracking-wide sm:text-xl md:text-left">
           <span>
             At{" "}
-            <span className="font-extrabold hover:underline tracking-tighter text-accent-500">
+            <span className="font-extrabold tracking-tighter text-accent-500 hover:underline">
               Hotelify
             </span>
             , we&rsquo;re committed to providing you with the best possible
@@ -15,13 +15,13 @@ function About() {
           </span>
           <span>
             Whether you&rsquo;re seeking a luxurious retreat, a budget-friendly
-            stay, or an adventure-filled escape, our platform offers a wide range
-            of accommodations to suit everyone&rsquo;s needs.
+            stay, or an adventure-filled escape, our platform offers a wide
+            range of accommodations to suit everyone&rsquo;s needs.
           </span>
         </p>
 
         {/* Image */}
-        <div className="flex-1 overflow-hidden rounded-xl md:max-h-[95%] aspect-[4/3]">
+        <div className="aspect-[4/3] flex-1 overflow-hidden rounded-xl md:max-h-[95%]">
           <img
             src="/hotel-images/img-2.jpg"
             alt="Hotel Room"
@@ -33,14 +33,14 @@ function About() {
       {/* Second Section */}
       <div className="flex min-h-[80vh] flex-col items-center gap-6 border-b-4 bg-slate-100 md:flex-row md:p-6">
         {/* Text Content */}
-        <p className="flex flex-1 flex-col gap-6 text-lg sm:text-xl leading-7 tracking-wide text-center md:text-left">
+        <p className="flex flex-1 flex-col gap-6 text-center text-lg leading-7 tracking-wide sm:text-xl md:text-left">
           <span>
             With easy navigation, secure payment options, and dedicated customer
             support, we strive to make your journey seamless and memorable.
           </span>
           <span>
             Your next unforgettable adventure awaits with{" "}
-            <span className="font-bold hover:underline tracking-tighter text-accent-500">
+            <span className="font-bold tracking-tighter text-accent-500 hover:underline">
               Hotelify
             </span>
             .
@@ -48,7 +48,7 @@ function About() {
         </p>
 
         {/* Image */}
-        <div className="flex-1 overflow-hidden rounded-xl bg-slate-800 md:max-h-[95%] aspect-[4/3]">
+        <div className="aspect-[4/3] flex-1 overflow-hidden rounded-xl bg-slate-800 md:max-h-[95%]">
           <img
             src="/hotel-images/img-3.jpg"
             alt="Hotel Lobby"
